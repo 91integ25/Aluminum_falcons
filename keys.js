6866c118769d7e470ffc25159858762aa4dc4472
r0bzter <robz.ms.qcf@gmail.com>
2017-05-13 11:14:25 -0400

    keys.js

---
+++ keys.js
@@ -0,0 +1,10 @@
+var CONSUMER_KEY = "KNRgIo5LpfIoK77bsy9VDORZr";
+var CONSUMER_SECRET = "mCu6uQUcpGgBi6Tnvq6nQ1uBZLZRxa3Jj662LjgVjlSvlNWiq1";
+var ACCESS_TOKEN = "788852777195671552-A4WTbe4Qr9KWI8DyrpqBl1XcGpdVXpJ";
+var ACCESS_SECRET = "nkiMAXR8od4OJioobmRz2PI19qaatxKhfRHR40LFBPjG2";
+module.exports = {
+    consumer_key: CONSUMER_KEY,
+    consumer_secret:CONSUMER_SECRET,
+    access_token: ACCESS_TOKEN,
+    access_secret: ACCESS_SECRET
+}
