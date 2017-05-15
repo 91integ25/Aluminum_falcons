8a8a0d0997b318d4ad6b3baea13e41bfd29ec6c8
Sandy Seay <seaysandra@bellsouth.net>
2017-05-14 22:23:43 -0400

    new user login working still working on signin

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,101 +6,128 @@ var db = require("../models");
 var bcrypt = require('bcrypt');
 var router = express.Router();
 var db = require("../models");
-var client = new Twitter({
-	consumer_key: keys.consumer_key,
-	consumer_secret: keys.consumer_secret,
-	access_token_key: keys.access_token,
-	access_token_secret: keys.access_secret
-});
-function tweetsData(company,cb){
-	console.log(company)
-		var stream =  client.stream('statuses/filter', {track: company});
-		var arr = [];
-		stream.on('data', function(event) {
-			arr.push(event.text);
-			if(arr.length === 20){
-				cb(arr);
-			}
-		});
-	}
-	function awsApi(cb,company){
-		var apiResults = []
-			tweetsData(company,function(tweetArr){
-				
-				for(var i = 0 ; i < tweetArr.length; i++){
-					unirest.post("https://twinword-sentiment-analysis.p.mashape.com/analyze/")
-					.header("X-Mashape-Key", "BOEwktCNBDmshSUeLunnuyGLz48wp1yHuyljsnNDWN4oLTDPPG")
-					.header("Content-Type", "application/x-www-form-urlencoded")
-					.header("Accept", "application/json")
-					.send("text="+tweetArr[i])
-					.end(function (result) {
-						apiResults.push(result.body);
-						
-						if(apiResults.length === i){
-							cb(apiResults);
-						}
+var jwt = require('jsonwebtoken');
 
-					});
-					
-				}
-			});
-	}
-
-module.exports = {
-	route: function(app){
-		app.post("/api/create_stock",function(req,res){
-			awsApi(function(data){
-				console.log("data arr: ",data);
-				//console.log("this worked",data);
-				var dataNum = data.map(function(e){
-					return Number(e.score);
-				})
-				var dataReduced = dataNum.reduce(function(a,b){
-					return a + b;
-				})
-				console.log(dataReduced/data.length);
-				
-				//console.log(req.body)
-				//res.json({company: req.body.company,sentiment:data});
-			},req.body.company)
-		});
-		app.post("/sign-up",function(req,res){
-			console.log(req.body)
-			db.User.create(req.body).then(function(){
-				res.json(req.body);
-			});
-		});
-		app.get("/log-in",function(req,res){
-			db.User.findOne({where:{
-				userName:req.body.userName
-			}}).then(function(result){
-				res.render("website",result);
-			})
-		});
-	}
-
-
-}
 var salt = '$2a$10$.zvkhL71NZo804bNdFdBae';
 
-router.get("/test", function(req, res) {
-  res.status(200).json({ 'message': 'Success'})
+var client = new Twitter({
+    consumer_key: keys.consumer_key,
+    consumer_secret: keys.consumer_secret,
+    access_token_key: keys.access_token,
+    access_token_secret: keys.access_secret
 });
 
+function tweetsData(company, cb) {
+    console.log(company)
+    var stream = client.stream('statuses/filter', { track: company });
+    var arr = [];
+    stream.on('data', function(event) {
+        arr.push(event.text);
+        if (arr.length === 20) {
+            cb(arr);
+        }
+    });
+}
+
+function awsApi(cb, company) {
+    var apiResults = []
+    tweetsData(company, function(tweetArr) {
+
+        for (var i = 0; i < tweetArr.length; i++) {
+            unirest.post("https://twinword-sentiment-analysis.p.mashape.com/analyze/")
+                .header("X-Mashape-Key", "BOEwktCNBDmshSUeLunnuyGLz48wp1yHuyljsnNDWN4oLTDPPG")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .header("Accept", "application/json")
+                .send("text=" + tweetArr[i])
+                .end(function(result) {
+                    apiResults.push(result.body);
+
+                    if (apiResults.length === i) {
+                        cb(apiResults);
+                    }
+
+                });
+
+        }
+    });
+};
+
+module.exports = {
+
+        route: function(app) {
+                app.get("/test", function(req, res) {
+                    res.status(200).json({ 'message': 'Success' })
+                });
 
 
-// POST route for creating a new user
-router.post("/user", function(req, res) {
-  bcrypt.hash(req.body.password, salt, function(err, hash) {
-    // Store hash in your password DB.
-    // TODO: update schema to enforce unique usernames
-    db.User.create({
-      username: req.body.username,
-      password: hash
-    })
-      .then(function(dbPost) {
-        res.status(200).json({'status': 'success'});
-      });
-  });
+                console.log(bcrypt.genSaltSync(10))
+                    // POST route for creating a new user changed apiRouter to app
+                    //TODO will app work without a var app
+                app.post("/user", function(req, res) {
+                    bcrypt.hash(req.body.password, salt, function(err, hash) {
+                        // Store hash in your password DB.
+                        // TODO: update schema to enforce unique usernames
+                        db.User.create({
+                                username: req.body.username,
+                                email: req.body.email,
+                                password: hash
+                            })
+                            .then(function(dbPost) {
+                                res.status(200).json({ 'status': 'success' });
+                            })
+                            .catch(function(err) {
+                                res.status(500).send(err);
+                            })
 
-});
+                    });
+
+                });
+                app.post("/user/signin", function(req, res) {
+                    db.User.findOne({
+                            username: req.body.username
+                        })
+                        .then(function(user) {
+                            if (!user) {
+                                console.log('no user found')
+                                res.status(400).json({
+                                    'status': 'Invalid username or password'
+                                })
+                            } else {
+                                bcrypt.compare(req.body.password, user.password, function(err, valid) {
+                                    if (err || !valid) {
+                                        res.status(400).json({
+                                            'status': 'Invalid username or password'
+                                        })
+                                    } else {
+                                        var userToken = jwt.sign({
+                                            //expires in one hour
+                                            exp: Math.floor(Date.now() / 1000) + (60 * 60),
+                                            data: user.id,
+                                        }, 'randomsecretforsigningjwt');
+                                        res.status(200).json({
+                                            id: user.id,
+                                            username: user.username,
+                                            token: userToken
+                                        });
+                                    }
+                                });
+                            }
+
+                        });
+                });
+                app.post("/api/create_stock", function(req, res){
+                    awsApi(function(data) {
+                        console.log("data arr: ", data)
+                        //console.log("this worked",data);
+                        var dataNum = data.map(function(e) {
+                            return Number(e.score);
+                        })
+                        var dataReduced = dataNum.reduce(function(a, b) {
+                            return a + b;
+                        })
+                        console.log(dataReduced / data.length)
+
+                        //console.log(req.body)
+                        //res.json({company: req.body.company,sentiment:data});
+	                     req.body.company});
+                });              
--- server.js
+++ server.js
@@ -7,21 +7,47 @@ var db = require("./models");
 var exphbs = require("express-handlebars");
 var apiRoutes = require("./routes/apiRoutes");
 var routes = require("./routes/htmlRoutes");
-
+var jwt = require('jsonwebtoken');
 
 
 app.use(express.static(process.cwd() + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(methodOverride("_method"));
 
 app.engine("handlebars",exphbs({defaultLayout:"main"}));
 app.set("view engine","handlebars");
 apiRoutes.route(app);
-app.use("/",routes)
+//app.use(app.router); no longer needed
+//routes.initialize(app);
 
-apiRoutes.route(app);
+//app.use('/api', apiRoutes);
+app.use('/api/secure', function (req, res, next) {
+  // check authorization
+  // if authorized next()
+  if (!req.header('Authorization')) {
+    res.status(401).json({ 'status': 'Not Authorized'});
+  } else {
+    jwt.verify(req.header('Authorization'), 'randomsecretforsigningjwt', function(err, decoded) {
+      if (err) {
+        console.log('err', err)
+        res.status(401).json({ 'status': 'Not Authorized'});
+      } else {
+        console.log(decoded.data) // bar
+        // query db for privileges for user
+        // add to req.privs
+        next();
+      }
+    });
+  }
+  // else res.status(401).json({})
+});
+//app.use('/api/secure', apiRoutes);
+
+//Syncing our sequelize models and then starting our express app
+
 db.sequelize.sync({force:true}).then(function(){
 	app.listen(PORT,function(){
 		console.log("listening on PORT: " + PORT)
