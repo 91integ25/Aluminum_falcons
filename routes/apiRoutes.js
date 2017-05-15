1e7b25eaadf2fc276e9c3d1e1daf7264448adba7
91integ25 <91integ25@gmail.com>
2017-05-14 20:49:40 -0400

    tweets working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,7 @@ var unirest = require("unirest");
 var express = require('express');
 var db = require("../models");
 var bcrypt = require('bcrypt');
-var router = express.Router();
+var salt = '$2a$10$.zvkhL71NZo804bNdFdBae';
 var db = require("../models");
 var client = new Twitter({
 	consumer_key: keys.consumer_key,
@@ -17,8 +17,10 @@ function tweetsData(company,cb){
 		var stream =  client.stream('statuses/filter', {track: company});
 		var arr = [];
 		stream.on('data', function(event) {
+			console.log(event.text)
 			arr.push(event.text);
-			if(arr.length === 20){
+			if(arr.length === 10){
+				console.log("error is not in twitter");
 				cb(arr);
 			}
 		});
@@ -34,10 +36,12 @@ function tweetsData(company,cb){
 					.header("Accept", "application/json")
 					.send("text="+tweetArr[i])
 					.end(function (result) {
+					
 						apiResults.push(result.body);
 						
 						if(apiResults.length === i){
 							cb(apiResults);
+								console.log("error is not in awsApi")
 						}
 
 					});
@@ -58,49 +62,50 @@ module.exports = {
 				var dataReduced = dataNum.reduce(function(a,b){
 					return a + b;
 				})
-				console.log(dataReduced/data.length);
-				
+				console.log(dataReduced, dataReduced/data.length);
+				res.send("this post worked");
 				//console.log(req.body)
 				//res.json({company: req.body.company,sentiment:data});
 			},req.body.company)
 		});
+
 		app.post("/sign-up",function(req,res){
 			console.log(req.body)
 			db.User.create(req.body).then(function(){
 				res.json(req.body);
 			});
 		});
+
 		app.get("/log-in",function(req,res){
-			db.User.findOne({where:{
+			db.User.findOne({
 				userName:req.body.userName
-			}}).then(function(result){
+			}).then(function(result){
 				res.render("website",result);
 			})
+		});
+
+		app.get("/test", function(req, res) {
+		  res.status(200).json({ 'message': 'Success'})
+		});
+		
+		// POST route for creating a new user
+		app.post("/user", function(req, res) {
+		  bcrypt.hash(req.body.password, salt, function(err, hash) {
+		    // Store hash in your password DB.
+		    // TODO: update schema to enforce unique usernames
+		    db.User.create({
+		      username: req.body.username,
+		      password: hash
+		    })
+		      .then(function(dbPost) {
+		        res.status(200).json({'status': 'success'});
+		      });
+		  });
+
 		});
 	}
 
 
 }
-var salt = '$2a$10$.zvkhL71NZo804bNdFdBae';
-
-router.get("/test", function(req, res) {
-  res.status(200).json({ 'message': 'Success'})
-});
 
 
-
-// POST route for creating a new user
-router.post("/user", function(req, res) {
-  bcrypt.hash(req.body.password, salt, function(err, hash) {
-    // Store hash in your password DB.
-    // TODO: update schema to enforce unique usernames
-    db.User.create({
-      username: req.body.username,
-      password: hash
-    })
-      .then(function(dbPost) {
-        res.status(200).json({'status': 'success'});
-      });
-  });
-
-});
