aeb755bbfa773be3ff2f8622d4e9b5931607bd5d
Phlongs <phlongs@gmail.com>
2017-05-16 18:21:02 -0400

    update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,24 @@
 var keys = require('../keys');
-// var Twitter = require('twitter');
-var twitter = require('ntwitter');
+var Twitter = require('twitter');
 var unirest = require("unirest");
-var app = require('express');
+var express = require('express');
 var db = require("../models");
 var bcrypt = require('bcrypt');
 var salt = '$2a$10$.zvkhL71NZo804bNdFdBae';
 var db = require("../models");
 var jwt = require('jsonwebtoken');
 var stream;
+var tweetCount;
 var monitoringCompany;
 var tweetTotalSentiment;
 
-var client = new twitter({
+var client = new Twitter({
     consumer_key: keys.consumer_key,
     consumer_secret: keys.consumer_secret,
     access_token_key: keys.access_token,
     access_token_secret: keys.access_secret
 });
+
 
 function tweetsData(company, cb) {
     console.log(company)
@@ -34,7 +35,6 @@ function tweetsData(company, cb) {
         }
     });
 }
-
 function beginMonitoring(company,cb) {
     // cleanup if we're re-setting the monitoring
     if (monitoringCompany) {
@@ -79,8 +79,8 @@ function beginMonitoring(company,cb) {
             });
             return stream;
 }
-//
-//
+
+
 function awsApi(cb, company) {
     var apiResults = []
     beginMonitoring(company, function(tweet) {
@@ -103,12 +103,12 @@ function awsApi(cb, company) {
         }
     });
 };
-//
+
 function resetMonitoring() {
 	if (stream) {
 		var tempStream = stream;
 	    stream = null;  // signal to event handlers to ignore end/destroy
-		tempStream.destroySilent();
+		tempStream.destroy();
 	}
 	  monitoringCompany = "";
 }
@@ -137,8 +137,7 @@ module.exports = {
 
      			});
 
-//
-//
+
 		app.post("/api/create_stock",function(req,res){
 			awsApi(function(data){
 				console.log("data arr: ",data);
@@ -155,79 +154,77 @@ module.exports = {
 				//res.json({company: req.body.company,sentiment:data});
 			},req.body.company)
 		});
-  }
+
+
+// 		app.post("/sign-up",function(req,res){
+// 			console.log(req.body)
+// 			db.User.create(req.body).then(function(){
+// 				res.json(req.body);
+// 			});
+// 		});
+
+// 		app.get("/log-in",function(req,res){
+// 			db.User.findOne({
+// 				userName:req.body.userName
+// 			}).then(function(result){
+// 				res.render("website",result);
+// 			})
+// 		});
+
+		// app.get("/test", function(req, res) {
+		//   res.status(200).json({ 'message': 'Success'})
+		// });
+
+		// // POST route for creating a new user
+		// app.post("/user", function(req, res) {
+		//   bcrypt.hash(req.body.password, salt, function(err, hash) {
+		//     // Store hash in your password DB.
+		//     // TODO: update schema to enforce unique usernames
+		//     db.User.create({
+		//       username: req.body.username,
+		//       password: hash
+		//     })
+		//       .then(function(dbPost) {
+		//         res.status(200).json({'status': 'success'});
+		//       });
+		//   });
+
+		// });
+			app.post("/user/signin", function(req, res) {
+                    db.User.findOne({
+                            username: req.body.username
+                        })
+                        .then(function(user) {
+                            if (!user) {
+                                console.log('no user found')
+                                res.status(400).json({
+                                    'status': 'Invalid username or password'
+                                })
+                            } else {
+                                bcrypt.compare(req.body.password, user.password, function(err, valid) {
+                                    if (err || !valid) {
+                                        res.status(400).json({
+                                            'status': 'Invalid username or password'
+                                        })
+                                    } else {
+                                        var userToken = jwt.sign({
+                                            //expires in one hour
+                                            exp: Math.floor(Date.now() / 1000) + (60 * 60),
+                                            data: user.id,
+                                        }, 'randomsecretforsigningjwt');
+                                        res.status(200).json({
+                                            id: user.id,
+                                            username: user.username,
+                                            token: userToken
+                                        });
+                                    }
+                                });
+                            }
+
+                        });
+                });
+
+
 }
-//
-//
-// 		// app.post("/sign-up",function(req,res){
-// 		// 	console.log(req.body)
-// 		// 	db.User.create(req.body).then(function(){
-// 		// 		res.json(req.body);
-// 		// 	});
-// 		// });
-//
-// // 		app.get("/log-in",function(req,res){
-// // 			db.User.findOne({
-// // 				userName:req.body.userName
-// // 			}).then(function(result){
-// // 				res.render("website",result);
-// // 			})
-// // 		});
-//
-// 		// app.get("/test", function(req, res) {
-// 		//   res.status(200).json({ 'message': 'Success'})
-// 		// });
-//
-// 		// // POST route for creating a new user
-// 		// app.post("/user", function(req, res) {
-// 		//   bcrypt.hash(req.body.password, salt, function(err, hash) {
-// 		//     // Store hash in your password DB.
-// 		//     // TODO: update schema to enforce unique usernames
-// 		//     db.User.create({
-// 		//       username: req.body.username,
-// 		//       password: hash
-// 		//     })
-// 		//       .then(function(dbPost) {
-// 		//         res.status(200).json({'status': 'success'});
-// 		//       });
-// 		//   });
-//
-// 		// });
-// 			app.post("/user/signin", function(req, res) {
-//                     db.User.findOne({
-//                             username: req.body.username
-//                         })
-//                         .then(function(user) {
-//                             if (!user) {
-//                                 console.log('no user found')
-//                                 res.status(400).json({
-//                                     'status': 'Invalid username or password'
-//                                 })
-//                             } else {
-//                                 bcrypt.compare(req.body.password, user.password, function(err, valid) {
-//                                     if (err || !valid) {
-//                                         res.status(400).json({
-//                                             'status': 'Invalid username or password'
-//                                         })
-//                                     } else {
-//                                         var userToken = jwt.sign({
-//                                             //expires in one hour
-//                                             exp: Math.floor(Date.now() / 1000) + (60 * 60),
-//                                             data: user.id,
-//                                         }, 'randomsecretforsigningjwt');
-//                                         res.status(200).json({
-//                                             id: user.id,
-//                                             username: user.username,
-//                                             token: userToken
-//                                         });
-//                                     }
-//                                 });
-//                             }
-//
-//                         });
-//                 });
-//
-//
-// }
-//
-// }
+
+}
