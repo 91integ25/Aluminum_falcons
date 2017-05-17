561e0c365c7a09fd0acf79d720a0a29368f9e2e4
r0bzter <robz.ms.qcf@gmail.com>
2017-05-16 20:42:55 -0400

    nodemailer

--- nodemailer/app.js
+++ nodemailer/app.js
@@ -6,16 +6,18 @@ var transporter = nodemailer.createTransport({
 	auth: {
 		xoauth2: xoauth2.createXOAuth2Generator({
 			user: 'robz.ms.qcf@gmail.com',
-			clientId: '714740077954-j1l45571phu2f5f933a64l7j4gd3vj6m.apps.googleusercontent.com',
-			clientSecret: 'TjnDZJgF-2af7t1w60uUBQYO',
-			refreshToken: '',
+			clientId: '714740077954-a0he0j6amaciht9r3ksotp1t60g24cgi.apps.googleusercontent.com',
+			clientSecret: 'HUDP1Oh_-hm_QLWo9OeEBFEQ',
+			refreshToken: '1/UkQU226UisXLbhmB_L1B38bRxljMftgFhkx-dIfUEgI'
 		})
 	}
+
+
 })
 
 var mailOptions = {
 	from: 'robz.ms.qcf@gmail.com',
-	to: '91integ25@gmail.com, robz.ms.qcf@gmail.com',
+	to: '91integ25@gmail.com',
 	subject: 'Nodemailer Test',
 	text: 'This should do the trick'
 }
